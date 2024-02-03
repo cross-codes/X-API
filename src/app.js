afa884f07f811131e650eaca66e43dfb4216e74c
Akshaj Rao <akshajrao2004@gmail.com>
2024-02-03 16:56:42 +0530

    fix(app.js): update cors config

--- src/app.js
+++ src/app.js
@@ -5,9 +5,14 @@ import "./db/connection.js";
 import tweetRouter from "./routers/tweet.router.js";
 import userRouter from "./routers/user.router.js";
 
+const corsOpts = {
+  origin: "http://localhost:8080",
+};
+
 const app = express();
 
-app.use(cors());
+app.options("*", cors(corsOpts));
+app.use(cors(corsOpts));
 
 app.use(express.json());
 app.use(userRouter);
